0c5864060ae7eae339d18c71a635e772844dcffb
Jared See <jiffy@Jareds-MacBook-Pro.local>
2023-10-24 16:26:18 -0400

    basic demo, little broken needs work

---
+++ map-polygon-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MapComponent from './google_map_component';
+
+function App() {
+  return (
+    <div className="App">
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
