4fdb68822cf1263fa4c7724b8dd62a7d7a588874
Chaseis4344 <Townsendchase78@yahoo.com>
2023-12-12 14:51:27 -0500

    Next JS migration Added

---
+++ map-prototype-trimble-nextjs/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import TrimbleMapComponent from './map'
+export default function Home() {
+  return (
+    <main className="">
+       <TrimbleMapComponent />
+    </main>
+  )
+}
