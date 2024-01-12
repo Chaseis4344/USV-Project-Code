48ed305d93661fd7dcf39ef258757929d702ff05
Adeline See <jiffy@Jareds-MacBook-Pro.local>
2024-01-12 11:57:21 -0500

    Delete unnecessary files and configurations

--- map-prototype-trimble-nextjs/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-import TrimbleMapComponent from './map'
-export default function Home() {
-  return (
-    <main className="">
-       <TrimbleMapComponent />
-    </main>
-  )
-}
