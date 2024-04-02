444892ccf00d81db40ba6953f2d11cf9242ae1be
Adeline See <jiffy@Jareds-MacBook-Pro.local>
2024-04-01 22:40:43 -0400

    Move save config button to right

--- ant-frontend/my-app/src/components/Configuration.js
+++ ant-frontend/my-app/src/components/Configuration.js
@@ -64,7 +64,7 @@ const Configuration = () => {
                                 defaultValue={30}
                             />
                         </Form.Item>
-                        <Form.Item>
+                        <Form.Item wrapperCol={{ span: 24, offset: 20 }}>
                             <Button type="primary" htmlType="submit">
                                 Save Configuration
                             </Button>
