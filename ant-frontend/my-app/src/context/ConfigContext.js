7363d84a6bbad461566d5a7c0593cc0b2a8410df
Chaseis4344 <Townsendchase78@yahoo.com>
2024-04-04 17:08:40 -0400

    feat: Axios requests to server from front-end

--- ant-frontend/my-app/src/context/ConfigContext.js
+++ ant-frontend/my-app/src/context/ConfigContext.js
@@ -1,22 +1,32 @@
 import React, { createContext, useState } from 'react';
 //const fs = require('fs');
 import config from '../config.json'
+import axios from 'axios';
  
 export const ConfigContext = createContext(null);
 export const ConfigProvider = ({ children }) => {
   const [config] = useState({});
 
-  const setConfig = (config) =>{
+  const setConfig = async (config) =>{
     //console.log('Configs Set:'+config);
    //localStorage.setItem('config', JSON.stringify(config,null,2));
-   console.error("Config Error: Cannot write to read-only resource")
+   
+   try{
+    const response = await axios.post(/*URL for resource goes here*/"", config);
+    }
+   catch(ex){
+    console.error("Config Error:"+ ex);
+     }
    
   }
   
 
-  const getConfig =() => {
+  const getConfig = async () => {
+    
+    
     try{
-     return config;
+      const response = await axios.get(/*URL goes here*/"");
+     return JSON.parse(response.data);
     }catch(ex){
       //Bad I/O Errors go here
       console.error(ex);
