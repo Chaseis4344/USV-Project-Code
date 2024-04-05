1e2f049f1d7e9908571fd2f8c7dd867ca890353f
Chaseis4344 <Townsendchase78@yahoo.com>
2024-04-05 10:44:03 -0400

    fix: config talking recoded

--- ant-frontend/my-app/src/context/ConfigContext.js
+++ ant-frontend/my-app/src/context/ConfigContext.js
@@ -1,6 +1,7 @@
 import React, { createContext, useState } from 'react';
 //const fs = require('fs');
 import axios from 'axios';
+import {API_URL} from '../config'
  
 export const ConfigContext = createContext(null);
 export const ConfigProvider = ({ children }) => {
@@ -8,7 +9,7 @@ export const ConfigProvider = ({ children }) => {
 
   const setConfig = async (config) =>{
    try{
-    const response = await axios.post(/*URL for resource goes here*/"", config);
+    const response = await axios.post("/api/config", config,{baseURL:API_URL});
     }
    catch(ex){
     console.error("Config Error:"+ ex);
@@ -19,8 +20,8 @@ export const ConfigProvider = ({ children }) => {
 
   const getConfig = async () => {
     try{
-      const response = await axios.get(/*URL goes here*/"");
-     return JSON.parse(response.data);
+      const response = await axios.get("/api/config",{baseURL:API_URL});
+     return response.data;
     }catch(ex){
       //Bad I/O Errors go here
       console.error(ex);
