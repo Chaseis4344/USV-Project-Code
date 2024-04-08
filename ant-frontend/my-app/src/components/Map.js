0d81aacb0bc910e44b6fe494a1a070a1b587c013
Adeline See <jareds2147@gmail.com>
2024-04-08 18:27:50 -0400

    Remove duplicate import statement in Map.js

--- ant-frontend/my-app/src/components/Map.js
+++ ant-frontend/my-app/src/components/Map.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import TrimbleMapsControl from './Drawtool';
 import { API_URL } from '../config';
-import { API_URL } from '../config';
 /* global TrimbleMapsControl */
 
 function TrimbleMapComponent() {
