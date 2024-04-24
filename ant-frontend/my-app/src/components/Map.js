c6d73c25c620a426384239438d3f34608d3ce24a
Adeline See <jareds2147@gmail.com>
2024-04-23 21:18:53 -0400

    api key from config and move data point thing to one file still broken tho

--- ant-frontend/my-app/src/components/Map.js
+++ ant-frontend/my-app/src/components/Map.js
@@ -2,109 +2,115 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import TrimbleMapsControl from './Drawtool';
 import { API_URL } from '../config';
-import TableInfo from './TableInfo';
+import TableComponent from './TableComponent';
+
 /* global TrimbleMapsControl */
 
 function TrimbleMapComponent() {
-    const [map, setMap] = useState(null);
-    const [draw, setDraw] = useState(null);
-    //const [surveyLinesMap] = useState(new Map());
-    const [waypoints, setWaypoints] = useState([]);
+  const [map, setMap] = useState(null);
+  const [draw, setDraw] = useState(null);
+  //const [surveyLinesMap] = useState(new Map());
+  const [waypoints, setWaypoints] = useState([]);
 
 
-    const extractWaypoints = (polygonData) => {
-        const waypoints = [];
-        polygonData.forEach(feature => {
-            if (feature.geometry.type === 'Polygon') {
-                feature.geometry.coordinates[0].forEach(coord => {
-                    waypoints.push({ lng: coord[0], lat: coord[1] });
-                });
-            }
+  const extractWaypoints = (polygonData) => {
+    const waypoints = [];
+    polygonData.forEach(feature => {
+      if (feature.geometry.type === 'Polygon') {
+        feature.geometry.coordinates[0].forEach(coord => {
+          waypoints.push({ lng: coord[0], lat: coord[1] });
         });
-        return waypoints; 
-    };
+      }
+    });
+    return waypoints;
+  };
 
 
-    const processPolygonData = async () => {
-        const polygonData = draw.getAll().features.filter(feature => feature.geometry.type === 'Polygon');
+  const processPolygonData = async () => {
+    const polygonData = draw.getAll().features.filter(feature => feature.geometry.type === 'Polygon');
 
-        if (polygonData.length > 0) {
-            try {
-                setWaypoints(polygonData[0].geometry.coordinates);
-                console.log('Sending polygon data to server:', polygonData[0].geometry.coordinates);
-                const response = await axios.post(`${API_URL}/api/process-polygon`, { polygonData });
-               // console.log('Server response:', response.data);
-                
-            } catch (error) {
-                console.error('Error sending polygon data to server:', error);
+    if (polygonData.length > 0) {
+      const coordinates = polygonData[0].geometry.coordinates[0];
+      const newWaypoints = coordinates.map(coord => ({ lng: coord[0], lat: coord[1] }));
+
+      setWaypoints(newWaypoints);
+
+      console.log('Waypoints:', newWaypoints);
+
+      try {
+        const response = await axios.post(`${API_URL}/api/process-polygon`, { polygonData });
+        // console.log('Server response:', response.data);
+      } catch (error) {
+        console.error('Error sending polygon data to server:', error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (!map) {
+      axios.get(`${API_URL}/api/config`)
+        .then(response => {
+          const apiKey = response.data.apiKey;
+
+          import('@trimblemaps/trimblemaps-js').then(TrimbleMaps => {
+            TrimbleMaps.setAPIKey(apiKey);
+
+            const newMap = new TrimbleMaps.Map({
+              container: "myMap",
+              center: new TrimbleMaps.LngLat(-81.84916796332335, 28.14888921518113),
+              zoom: 15,
+              region: TrimbleMaps.Common.Region.NA3,
+              style: TrimbleMaps.Common.Style.SATELLITE,
+              language: TrimbleMaps.Common.Language.EN,
+              hash: true,
+              satelliteProvider: TrimbleMaps.Common.SatelliteProvider.SAT6,
+            });
+
+            setMap(newMap);
+
+            if (typeof TrimbleMapsControl !== 'undefined') {
+              const newDraw = new TrimbleMapsControl.Draw({
+                displayControlsDefault: false,
+                controls: {
+                  polygon: true,
+                  trash: true,
+                  save: true,
+                  upload: true
+                }
+              });
+
+              newMap.addControl(newDraw, "top-left");
+              setDraw(newDraw);
             }
+          }).catch(error => console.error("Failed to load TrimbleMaps", error));
+        })
+        .catch(error => console.error("Failed to retrieve config", error));
+    }
+  }, [map]);
+
+  useEffect(() => {
+    if (map && draw) {
+      const handlePolygonComplete = (e) => {
+        const shape = e.features[0];
+        if (shape.geometry.type === 'Polygon') {
+          processPolygonData();
         }
-    };
+      };
 
-    useEffect(() => {
-        if (!map) {
-            import('@trimblemaps/trimblemaps-js').then(TrimbleMaps => {
-                TrimbleMaps.setAPIKey("E006198D3D2D034197622ADE3E8DF111");
+      map.on('draw.create', handlePolygonComplete);
 
-                const newMap = new TrimbleMaps.Map({
-                    container: "myMap",
-                    center: new TrimbleMaps.LngLat(-81.84916796332335, 28.14888921518113),
-                    zoom: 15,
-                    region: TrimbleMaps.Common.Region.NA3,
-                    style: TrimbleMaps.Common.Style.SATELLITE,
-                    language: TrimbleMaps.Common.Language.EN,
-                    hash: true,
-                    satelliteProvider: TrimbleMaps.Common.SatelliteProvider.SAT6,
-                });
+      return () => {
+        map.off('draw.create', handlePolygonComplete);
+      };
+    }
+  }, [map, draw, processPolygonData]);
 
-                setMap(newMap);
-
-                if (typeof TrimbleMapsControl !== 'undefined') {
-                    const newDraw = new TrimbleMapsControl.Draw({
-                        displayControlsDefault: false,
-                        controls: {
-                          polygon: true,
-                          trash: true,
-                          save: true,
-                          upload: true
-                        }
-                      });
-                      newMap.addControl(newDraw, "top-left");
-                      setDraw(newDraw);
-                }
-            }).catch(error => console.error("Failed to load TrimbleMaps", error));
-        }
-    }, [map]);
-
-    useEffect(() => {
-        if (map && draw) {
-            const handlePolygonComplete = (e) => {
-                const shape = e.features[0];
-                if (shape.geometry.type === 'Polygon') {
-                    processPolygonData();
-                }
-            };
-
-            const handlePolygonUpdate = (e) => {
-                processPolygonData();
-            };
-
-            map.on('draw.create', handlePolygonComplete);
-            map.on('draw.update', handlePolygonUpdate);
-
-            return () => {
-                map.off('draw.create', handlePolygonComplete);
-                map.off('draw.update', handlePolygonUpdate);
-            };
-        }
-    }, [map, draw, processPolygonData]); 
-
-    return (
-        <div style={{ position: 'fixed', top: 0, left: 200, right: 0, bottom: 0 }}>
-            <div id="myMap" style={{ width: '100%', height: '100%' }}></div>
-            <TableInfo waypoints={waypoints} /> 
-        </div>
-    );
+  return (
+    <div style={{ position: 'fixed', top: 0, left: 200, right: 0, bottom: 0 }}>
+      <div id="myMap" style={{ width: '100%', height: '100%' }}></div>
+      {waypoints.length > 0 && <TableComponent waypoints={waypoints} />}
+    </div>
+  );
 }
 
 export default TrimbleMapComponent;
--- ant-frontend/my-app/src/components/TableComponent.js
+++ ant-frontend/my-app/src/components/TableComponent.js
@@ -1,25 +1,97 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table, Tooltip } from 'antd';
 import { TableOutlined } from '@ant-design/icons';
-import { Tooltip } from 'antd';
-import TableInfo from './TableInfo';
+import '../App.css';
+import './Map';
 
-
-const TableComponent = () => {
+const TableComponent = ({ waypoints }) => {
   const [visible, setVisible] = useState(false);
-  
+  const [selectionType] = useState('checkbox');
+  const [dataSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    if (waypoints) {
+      const updatedDataSource = waypoints.map((waypoint, index) => ({
+        key: index + 1,
+        waypoint: `Waypoint ${index + 1}`,
+        latitude: waypoint.lat,
+        longitude: waypoint.lng,
+      }));
+      setDataSource(updatedDataSource);
+    }
+  }, [waypoints]);
+
   const toggleVisibility = () => {
     setVisible(!visible);
   };
-  
-  
+
+  const rowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+      console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+    },
+  };
+
+  const columns = [
+    {
+      title: 'Waypoint',
+      dataIndex: 'waypoint',
+      key: 'waypoint',
+    },
+    {
+      title: 'Latitude',
+      dataIndex: 'latitude',
+      key: 'latitude',
+    },
+    {
+      title: 'Longitude',
+      dataIndex: 'longitude',
+      key: 'longitude',
+    },
+  ];
+
+  console.log("Data:" + JSON.stringify(dataSource));
+
   return (
     <div>
-      <div style={{ display: visible ? 'block' : 'none', borderRadius: '5%', position: 'fixed', bottom: 110, right: 10, zIndex: 999, background: '#fff', padding: '10px', border: '1px solid #1677FF' }}>
-        <TableInfo/>
-      </div>
+      {visible && (
+        <div
+          style={{
+            borderRadius: '5%',
+            position: 'fixed',
+            bottom: 110,
+            right: 10,
+            zIndex: 999,
+            background: '#fff',
+            padding: '10px',
+            border: '1px solid #1677FF',
+          }}
+        >
+          <Table
+            columns={columns}
+            dataSource={dataSource}
+            rowSelection={{
+              type: selectionType,
+              ...rowSelection,
+            }}
+          />
+        </div>
+      )}
+
       <div style={{ position: 'fixed', bottom: 50, right: 0, zIndex: 999 }}>
         <Tooltip title="Waypoint Data">
-          <div style={{ width: 50, height: 50, borderRadius: '10%', background: '#1677FF', textAlign: 'center', lineHeight: '60px', cursor: 'pointer', color: '#fff' }} onClick={toggleVisibility}>
+          <div
+            style={{
+              width: 50,
+              height: 50,
+              borderRadius: '10%',
+              background: '#1677FF',
+              textAlign: 'center',
+              lineHeight: '60px',
+              cursor: 'pointer',
+              color: '#fff',
+            }}
+            onClick={toggleVisibility}
+          >
             <TableOutlined style={{ fontSize: '30px' }} />
           </div>
         </Tooltip>
@@ -28,4 +100,4 @@ const TableComponent = () => {
   );
 };
 
-  export default TableComponent;
+export default TableComponent;
